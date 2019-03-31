7ed1d7bf03684c816f2ee31a90b7cd1291b83e8d
QZB19950217 <1053906294@qq.com>
2019-04-01 06:59:18 +1100

    Add files via upload

---
+++ week 5/DRL-SA.py
@@ -0,0 +1,254 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 24 15:13:00 2019
+@author: Xianglin ZHOU
+Q learning agent with fake goal and observer
+"""
+
+import numpy as np
+import pandas as pd
+import time
+import copy
+import tkinter as tk
+
+#actions
+ACTIONS = ['LEFT', 'RIGHT', 'UP', 'DOWN']
+ID_ACTIONS = list(range(len(ACTIONS)))# 0:left, 1:right, 3:up, 4:down
+#q_learning
+GAMMA = 0.9
+ALPHA = 0.1
+EPSILON = 0.9
+LAMBDA = 0.9
+EPISODES = 30
+#map
+SIZE = 100
+X = 9
+Y = 9
+WALLS = [[-X, -1], [-1, Y], [X, Y], [X, -1]]
+for i in range(0, X):
+    WALLS = WALLS + [[i , -1]] + [[-1, i]] + [[i, Y]] + [[X, i]]
+BARRIERS = [] + WALLS
+#print(len(BARRIERS))
+#GOALS = [[5,5]]
+START = [1,0]
+FAKE = [[0,7]] #fake goal
+GOALS = [[7,5]]
+        
+class Maze(tk.Tk, object):
+    def __init__(self, size, x, y):
+        super(Maze, self).__init__()
+        self.title('maze')              
+        self.goals = GOALS
+        self.fake = FAKE
+        self.barriers = BARRIERS
+        self.size = size
+        self.x_total = x
+        self.y_total = y
+        self.geometry('1800x900')
+        self.start = START
+        self._build_maze()
+        
+        
+    def _build_maze(self):
+        self.canvas = tk.Canvas(self, bg = 'white', height = self.size * self.y_total, 
+                                width = self.size * self.x_total)
+        
+        for i in range(self.x_total):
+            for j in range(5):
+                self.canvas.create_line(self.size - 2 + j + self.size*i, 0, 
+                                        self.size - 2 + j + self.size*i, self.size*self.y_total)
+                
+        for i in range(self.y_total):
+            for j in range(5):
+                self.canvas.create_line(0, self.size - 2 + j + self.size*i,
+                                        self.size*self.y_total, self.size - 2 + j + self.size*i)
+
+#        mouse_file = tk.PhotoImage(file='mouse.gif')
+#        self.mouse = self.canvas.create_image(10, 10, anchor='nw', image = mouse_file)
+#        print(11111)
+#        food_file = tk.PhotoImage(file='food.gif')
+#        self.food = self.canvas.create_image(1510, 10, anchor='nw', image = food_file)
+        self.food = self.canvas.create_rectangle(10 + self.goals[0][0]*self.size, 10 + self.goals[0][1]*self.size,(self.goals[0][0] + 1)*self.size - 10, (self.goals[0][1] + 1)*self.size - 10,fill = 'red')
+        self.fakefood = self.canvas.create_rectangle(10 + self.fake[0][0]*self.size, 10 + self.fake[0][1]*self.size,(self.fake[0][0] + 1)*self.size - 10, (self.fake[0][1] + 1)*self.size - 10,fill = 'orange')
+        self.mouse = self.canvas.create_oval(10 + self.start[0]*self.size, 10 + self.start[1]*self.size, 10 + self.start[0]*self.size + self.size - 20, 10 +self.start[1]*self.size + self.size - 20, fill = 'black')
+#       self.barriers = self.canvas.create_rectangle(10 + self.barriers[0][0]*self.size, 10 + self.barriers[0][1]*self.size,(self.barriers[0][0] + 1)*self.size - 10, (self.barriers[0][1] + 1)*self.size - 10,fill = 'gray')
+        # pack all
+        self.canvas.pack()
+
+    def reset(self):
+        self.update()
+        time.sleep(0.5)
+        self.canvas.delete(self.mouse)
+        self.mouse = self.canvas.create_oval(10 + self.start[0]*self.size, 10 + self.start[1]*self.size, 10 + self.start[0]*self.size + self.size - 20, 10 +self.start[1]*self.size + self.size - 20, fill = 'black')
+#        mouse_file = tk.PhotoImage(file='mouse.gif')
+#        self.mouse = self.canvas.create_image(10, 10, anchor='nw', image = mouse_file)
+        # return observation
+        #return self.canvas.coords(self.rect)
+        
+    def move_to(self, action):
+        self.update()
+        time.sleep(0.02)
+        if action == ID_ACTIONS[0]:
+            self.canvas.move(self.mouse, -self.size, 0)
+        elif action == ID_ACTIONS[1]:
+            self.canvas.move(self.mouse, self.size, 0)
+        elif action == ID_ACTIONS[2]:
+            self.canvas.move(self.mouse, 0, -self.size)
+        else:
+            self.canvas.move(self.mouse, 0, self.size)
+            
+    def render(self):
+        time.sleep(0.1)
+        self.update()
+    
+    def env_reaction(self, agent, state, action, show):
+        new_state = copy.copy(state)
+        #new_state = state
+        if action == ID_ACTIONS[0]:
+            new_state[0] -= 1
+        elif action == ID_ACTIONS[1]:
+            new_state[0] += 1
+        elif action == ID_ACTIONS[2]:
+            new_state[1] -= 1
+        else:
+            new_state[1] += 1
+            
+        if new_state in BARRIERS:
+            new_state = state
+        elif show:
+            self.move_to(action)
+        if new_state not in agent.goals:
+            r = 0
+        else:
+            r = 1
+        return new_state, r
+
+
+class SARSA_Agent:
+    def __init__(self, observation, actions = ID_ACTIONS, learning_rate = ALPHA, reward_decay = GAMMA, e_greedy = EPSILON):
+        self.actions = actions 
+        self.lr = learning_rate
+        self.gamma = reward_decay
+        self.epsilon = e_greedy
+        self.q_table = pd.DataFrame(columns=self.actions, dtype=np.float64)
+        self.observation = observation
+        if observation:
+            self.goals = FAKE
+        else:
+            self.goals = GOALS
+        
+    def choose_action(self, state):
+        self.check_state_exist(state)
+        if np.random.uniform() < self.epsilon:
+            scores_of_actions = self.q_table.loc[state, :]
+            action = np.random.choice(scores_of_actions[scores_of_actions 
+                                                        == np.max(scores_of_actions)].index)
+        else:
+            action = np.random.choice(self.actions)
+        return action
+
+    def myReward (self, state, action):
+        self.check_state_exist(state)
+        scores_of_actions = self.q_table.loc[state, :]
+        myActions = scores_of_actions[scores_of_actions == np.max(scores_of_actions)].index
+        #print(action, myActions)
+        r = -0.01
+        if (action in myActions):
+            r = 0
+        return r
+
+    def check_state_exist(self, state):
+        if state not in self.q_table.index:
+            # append new state to q table
+            self.q_table = self.q_table.append(
+                pd.Series(
+                    [0]*len(self.actions),
+                    index = self.q_table.columns,
+                    name = state,
+                )
+            )
+                
+    def learn(self, state, action, r, new_state, new_action):
+        self.check_state_exist(new_state)
+        q_predict = self.q_table.loc[state, action]
+        if new_state not in GOALS:
+            q_target = r + self.gamma * self.q_table.loc[new_state, new_action]
+        else:
+            q_target = r
+        self.q_table.loc[state, action] += self.lr * (q_target - q_predict)
+
+
+def ini_agent(agent):
+    env.reset()
+    state = START
+    action = agent.choose_action(str(state))
+    while state not in agent.goals:
+        new_state, r = env.env_reaction(agent, state, action, False)
+        new_action = agent.choose_action(str(new_state))
+        agent.learn(str(state), action, r, str(new_state), new_action)
+        state = new_state
+        action = new_action
+
+            
+    #return q_table
+def iniTraining(agent):
+    for t in range(2*EPISODES):
+        env.reset()
+        env.render()
+        ini_agent(agent)
+    env.reset()
+
+def run_agent(agent, observer):
+    env.reset()
+    state = START
+    action = agent.choose_action(str(state))
+    while state not in agent.goals:
+        action = agent.choose_action(str(state))
+        new_state, r = env.env_reaction(agent, state, action, False)
+        new_action = agent.choose_action(str(new_state))
+        r += observer.myReward(str(state),action)
+        #print (r)
+        agent.learn(str(state), action, r, str(new_state), new_action)
+        state = new_state
+        action = new_action
+
+def training(agent, observer):
+    for t in range(EPISODES//2):
+        env.reset()
+        env.render()
+        for j in range(EPISODES//2):
+            run_agent (agent, observer)
+            run_agent (observer, agent)
+            print ( j+1, " adversial episode finished")
+    print (agent.q_table)
+
+def show(agent):
+    for t in range (EPISODES):
+        env.reset()
+        env.render()
+        state = START
+        while state not in agent.goals:
+            action = agent.choose_action(str(state))
+            new_state, r = env.env_reaction(agent, state, action, True)
+            state = new_state
+
+if __name__ == "__main__":
+    env = Maze(SIZE, X, Y)
+    RL = SARSA_Agent(False)
+    OB = SARSA_Agent(True)
+    RL.epsilon = 0
+    OB.epsilon = 0
+    iniTraining(RL)
+    print ("RL trained")
+    print (RL.q_table)
+    iniTraining(OB)
+    print ("OB trained")
+    print (OB.q_table)
+    #env.after(100, update)
+    RL.epsilon = EPSILON
+    OB.epsilon = EPSILON
+    training(RL,OB)
+    RL.epsilon = 2
+    show(RL)
+    
+env.mainloop()
