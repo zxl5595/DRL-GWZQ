5fe2035dfcfbf144744e566da3b972cc4b7ccc11
Xiangg4 <48424436+Xiangg4@users.noreply.github.com>
2019-05-11 20:13:59 +1000

    Add files via upload

---
+++ week 9/cycle.py
@@ -0,0 +1,357 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 24 15:13:00 2019
+@author: Zhibo QU
+Q learning agent with fake goal and observer, using simulation strategy.
+"""
+
+import numpy as np
+import pandas as pd
+import time
+import copy
+import tkinter as tk
+
+#actions
+ACTIONS = ['LEFT', 'RIGHT', 'UP', 'DOWN']
+ID_ACTIONS = list(range(len(ACTIONS)))# 0:left, 1:right, 2:up, 3:down
+#q_learning
+GAMMA = 0.9
+ALPHA = 0.1
+EPSILON = 0.9
+LAMBDA = 0.9
+EPISODES = 30
+#map
+SIZE = 100
+X = 9
+Y = 9
+WALLS = [[-X, -1], [-1, Y], [X, Y], [X, -1]]
+OBS = []
+for i in range(0, X):
+    WALLS = WALLS + [[i , -1]] + [[-1, i]] + [[i, Y]] + [[X, i]]
+BARRIERS = OBS + WALLS
+#print(len(BARRIERS))
+#GOALS = [[5,5]]
+def distance (position1, position2):
+    dist = abs(position1[0] - position2[0]) + abs(position1[1]-position2[1])
+    return dist
+while True:
+    random_x = np.random.randint(0, X-1, 3)
+    random_y = np.random.randint(0, Y-1, 3)
+    random_s = [random_x[0], random_y[0]]
+    random_f = [random_x[1], random_y[1]]
+    random_r = [random_x[2], random_y[2]]
+    random_list = [random_s, random_f, random_r]
+    """
+    for i in range(len(random_list)):
+        if random_list.count(random_list[i]) == 1:
+            pass
+        else:
+            continue
+    """
+    if (distance(random_list[0],random_list[1])>=(X+Y)/3) and (distance(random_list[1],random_list[2])>=(X+Y)/3) and (distance(random_list[0],random_list[2])>=(X+Y)/3):
+        for i in range(len(random_list)):
+            if random_list.count(random_list[i]) == 1:
+                pass
+            else:
+                continue
+    else:
+        continue
+    break
+    
+START = random_s
+FAKE = [random_f] #fake goal
+GOALS = [random_r]
+
+        
+class Maze(tk.Tk, object):
+    def __init__(self, size, x, y, start = START, fake = FAKE, goals = GOALS):
+        super(Maze, self).__init__()
+        self.title('maze')              
+        self.goals = goals
+        self.fake = fake
+        self.obstacle = OBS
+        self.barriers = BARRIERS
+        self.size = size
+        self.x_total = x
+        self.y_total = y
+        self.geometry('1800x900')
+        self.start = start
+        self._build_maze()
+        
+        
+    def _build_maze(self):
+        self.canvas = tk.Canvas(self, bg = 'white', height = self.size * self.y_total, 
+                                width = self.size * self.x_total)
+        
+        for i in range(self.x_total):
+            for j in range(5):
+                self.canvas.create_line(self.size - 2 + j + self.size*i, 0, 
+                                        self.size - 2 + j + self.size*i, self.size*self.y_total)
+                
+        for i in range(self.y_total):
+            for j in range(5):
+                self.canvas.create_line(0, self.size - 2 + j + self.size*i,
+                                        self.size*self.y_total, self.size - 2 + j + self.size*i)
+
+#        mouse_file = tk.PhotoImage(file='mouse.gif')
+#        self.mouse = self.canvas.create_image(10, 10, anchor='nw', image = mouse_file)
+#        print(11111)
+#        food_file = tk.PhotoImage(file='food.gif')
+#        self.food = self.canvas.create_image(1510, 10, anchor='nw', image = food_file)
+        self.food = self.canvas.create_rectangle(10 + self.goals[0][0]*self.size, 10 + self.goals[0][1]*self.size,(self.goals[0][0] + 1)*self.size - 10, (self.goals[0][1] + 1)*self.size - 10,fill = 'red')
+        self.fakefood = self.canvas.create_rectangle(10 + self.fake[0][0]*self.size, 10 + self.fake[0][1]*self.size,(self.fake[0][0] + 1)*self.size - 10, (self.fake[0][1] + 1)*self.size - 10,fill = 'orange')
+        self.mouse = self.canvas.create_oval(10 + self.start[0]*self.size, 10 + self.start[1]*self.size, 10 + self.start[0]*self.size + self.size - 20, 10 +self.start[1]*self.size + self.size - 20, fill = 'black')
+        if (len(self.obstacle)>0):
+            self.ob = []
+            for i in range(len(self.obstacle)):
+                self.ob.append(self.canvas.create_rectangle(10 + self.obstacle[i][0]*self.size, 10 + self.obstacle[i][1]*self.size,(self.obstacle[i][0] + 1)*self.size - 10, (self.obstacle[i][1] + 1)*self.size - 10,fill = 'gray'))
+        # pack all
+        self.canvas.pack()
+
+    def reset(self):
+        self.update()
+        time.sleep(0.01)
+        self.canvas.delete(self.mouse)
+        self.mouse = self.canvas.create_oval(10 + self.start[0]*self.size, 10 + self.start[1]*self.size, 10 + self.start[0]*self.size + self.size - 20, 10 +self.start[1]*self.size + self.size - 20, fill = 'black')
+#        mouse_file = tk.PhotoImage(file='mouse.gif')
+#        self.mouse = self.canvas.create_image(10, 10, anchor='nw', image = mouse_file)
+        # return observation
+        #return self.canvas.coords(self.rect)
+        
+    def move_to(self, action):
+        self.update()
+        time.sleep(0.03)
+        if action == ID_ACTIONS[0]:
+            self.canvas.move(self.mouse, -self.size, 0)
+        elif action == ID_ACTIONS[1]:
+            self.canvas.move(self.mouse, self.size, 0)
+        elif action == ID_ACTIONS[2]:
+            self.canvas.move(self.mouse, 0, -self.size)
+        else:
+            self.canvas.move(self.mouse, 0, self.size)
+            
+    def render(self):
+        time.sleep(0.01)
+        self.update()
+
+        
+    def getSuccessor(self, state):
+        new_state = copy.copy(state)
+        left = [new_state[0] - 1,new_state[1]]
+        right = [new_state[0] + 1,new_state[1]]
+        up = [new_state[0],new_state[1] - 1]
+        down = [new_state[0],new_state[1] + 1]
+        if len(new_state) > 2:
+            left = left + [True]
+            right = right + [True]
+            up = up + [True]
+            down = down + [True]			
+        return [left,right,up,down]
+
+    def hitBarrier(self,state,action):
+        suc = self.getSuccessor(state)
+        new_state = [suc[action][0],suc[action][1]]
+        if new_state in BARRIERS:
+            return True
+        return False
+
+    def env_reaction(self, agent, state, action, show):
+        suc = self.getSuccessor(state)
+        new_state = suc[action]
+        if new_state not in agent.goals:
+            r = 0
+        else:
+            r = 1            
+        if show:
+            self.move_to(action)
+        return new_state, r
+
+
+
+class Qlearning_Agent:
+    def __init__(self, observation, actions = ID_ACTIONS, learning_rate = ALPHA, reward_decay = GAMMA, e_greedy = EPSILON):
+        self.actions = actions 
+        self.lr = learning_rate
+        self.gamma = reward_decay
+        self.epsilon = e_greedy
+        self.q_table = pd.DataFrame(columns=self.actions, dtype=np.float64)
+        self.observation = observation
+        if observation:
+            self.goals = FAKE
+        else:
+            self.goals = GOALS
+        
+    def choose_action(self, state):
+        self.check_state_exist(state)
+        if np.random.uniform() < self.epsilon:
+            scores_of_actions = self.q_table.loc[state, :]
+            action = np.random.choice(scores_of_actions[scores_of_actions 
+                                                        == np.max(scores_of_actions)].index)
+        else:
+            action = np.random.choice(self.actions)
+        return action
+
+    def myReward (self, state, action):
+        self.check_state_exist(state)
+        scores_of_actions = self.q_table.loc[state, :]
+        myActions = scores_of_actions[scores_of_actions == np.max(scores_of_actions)].index
+        #print(action, myActions)
+        r = -0.2
+        if (action in myActions):
+            r = 0.5
+        return r
+
+    def check_state_exist(self, state):
+        if state not in self.q_table.index:
+            # append new state to q table
+            self.q_table = self.q_table.append(
+                pd.Series(
+                    [0]*len(self.actions),
+                    index = self.q_table.columns,
+                    name = state,
+                )
+            )
+                
+    def learn(self, state, action, r, new_state):
+        self.check_state_exist(new_state)
+        q_predict = self.q_table.loc[state, action]
+        if new_state not in self.goals:
+            q_target = r + self.gamma * self.q_table.loc[new_state, :].max()
+        else:
+            q_target = r  # next state is terminal
+        self.q_table.loc[state, action] += self.lr * (q_target - q_predict)
+
+        
+
+def ini_agent(agent):
+
+    state = FAKE[0] + [True]
+    if agent.observation:
+        state = START
+    while state not in agent.goals:
+        action = agent.choose_action(str(state))
+        while env.hitBarrier (state,action):
+            action = agent.choose_action(str(state))
+        new_state, r = env.env_reaction(agent, state, action, False)
+        agent.learn(str(state), action, r, str(new_state))
+        state = new_state
+
+
+def iniTraining(agent):
+    for t in range(3*EPISODES):
+        ini_agent(agent)
+
+
+def run_agent(agent, observer):
+    state = START
+    while state not in observer.goals:
+        action = observer.choose_action(str(state))
+        while env.hitBarrier (state,action):
+            action = observer.choose_action(str(state))
+        new_state, r = env.env_reaction(agent, state, action, False)
+        r += observer.myReward(str(state),action)
+        agent.learn(str(state), action, r, str(new_state))
+        state = new_state
+    state = state + [True]
+    while state not in agent.goals:
+        action = agent.choose_action(str(state))
+        while env.hitBarrier (state,action):
+            action = agent.choose_action(str(state))
+        new_state, r = env.env_reaction(agent, state, action, False)
+        agent.learn(str(state), action, r, str(new_state))
+        state = new_state
+
+def training(agent, observer):
+    t = 0
+    while (True):
+        show (agent,1)
+        run_agent (agent, observer)
+        #run_agent (observer, agent)
+        t += 1
+        print(t, " completed")
+
+
+def show(agent,e):
+    agent.epsilon = 2
+    for t in range (e):
+        env.reset()
+        env.render()
+        state = START
+        visited = []
+        duplicated = []
+        while state not in agent.goals:
+            action = agent.choose_action(str(state))
+            new_state, r = env.env_reaction(agent, state, action, True)
+            state = new_state
+            if state in FAKE:
+                state = state + [True]
+            if state not in visited:
+                 visited.append(state)
+            elif state not in duplicated:
+                duplicated.append(state)
+            else:
+                env.reset()
+                break
+            
+    agent.epsilon = EPSILON
+    env.reset()
+
+if __name__ == "__main__":    
+    for t in range(5):
+        while True:
+            random_x = np.random.randint(0, X-1, 3)
+            random_y = np.random.randint(0, Y-1, 3)
+            random_s = [random_x[0], random_y[0]]
+            random_f = [random_x[1], random_y[1]]
+            random_r = [random_x[2], random_y[2]]
+            random_list = [random_s, random_f, random_r]
+            """
+            for i in range(len(random_list)):
+                if random_list.count(random_list[i]) == 1:
+                    pass
+                else:
+                    continue
+            """
+            if (distance(random_list[0],random_list[1])>=(X+Y)/3) and (distance(random_list[1],random_list[2])>=(X+Y)/3) and (distance(random_list[0],random_list[2])>=(X+Y)/3):
+                for i in range(len(random_list)):
+                    if random_list.count(random_list[i]) == 1:
+                        pass
+                    else:
+                        continue
+            else:
+                continue
+            break
+            
+        start = random_s
+        fake = [random_f] #fake goal
+        goals = [random_r]
+        env = Maze(SIZE, X, Y, start, fake, goals)
+        
+        RL = Qlearning_Agent(False)
+#        OB = Qlearning_Agent(True)
+#        RL.epsilon = 0
+#        OB.epsilon = 0
+#        print ("Initializing...")
+#        iniTraining(RL)
+#        print (RL.q_table)
+#        iniTraining(OB)
+#        print (OB.q_table)
+#        RL.epsilon = EPSILON
+#        OB.epsilon = EPSILON
+#        training(RL,OB)
+        show(RL,1)
+        print('env.start, env.fake, env.goals')
+        result = str(env.start) + str(env.fake) + str(env.goals)
+        print(result)
+        f = open('result.txt', 'a')
+        f.write(str(t))
+        f.write('th\n')
+        f.write(result)
+        f.write('\n')
+        f.close()
+        env.destroy()
+        print("destroyed")
+        env.mainloop()
+
+    
+    
